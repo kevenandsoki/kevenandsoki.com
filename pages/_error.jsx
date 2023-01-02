4adda9ed263f753038616545d60ef90cdb53ef2e
kevenandsoki <53108002+kevenandsoki@users.noreply.github.com>
2023-01-02 17:25:58 -0500

    Errors redirect to home

---
+++ pages/_error.jsx
@@ -0,0 +1,12 @@
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+
+export default function Error({ Component, pageProps }) {
+	const router = useRouter();
+
+	useEffect(() => {
+		router.replace('/');
+	}, []);
+
+	return null;
+}
