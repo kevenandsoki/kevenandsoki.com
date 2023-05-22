701477b8d9f5f8e826461851bfc2d22ba3c408f0
kevenandsoki <53108002+kevenandsoki@users.noreply.github.com>
2023-05-22 17:27:49 -0400

    Changed front page description for good.

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,7 +4,7 @@ export default function Home() {
 	return (
 		<>
 			<TextBox portraitURL="/assets/keven_portrait.png">
-				hi, i'm keven. i'm a fusion composer and producer, inspired by digifu and VGM.
+				hi, i'm keven. i'm a fusion-jazz music composer and producer, specializing in video game music.
 			</TextBox>
 			<TextBox>
 				you can find various examples of my works, projects that i've worked on, and some info about me on this site.
