4556bb9ed098638cf79209a097d8732e245696be
kevenandsoki <53108002+kevenandsoki@users.noreply.github.com>
2023-07-09 16:30:12 -0400

    Redid home page text again...

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,7 +4,7 @@ export default function Home() {
 	return (
 		<>
 			<TextBox portraitURL="/assets/keven_portrait.png">
-				hi, i'm keven. i'm a fusion-jazz music composer and producer, specializing in video game music.
+				hi, i'm keven. i'm a video game music composer and producer, inspired by fusion-jazz.
 			</TextBox>
 			<TextBox>
 				you can find various examples of my works, projects that i've worked on, and some info about me on this site.
