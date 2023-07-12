e46263ba362ebd9831c1ff4dda1a5cb04f7b5027
kevenandsoki <53108002+kevenandsoki@users.noreply.github.com>
2023-07-11 23:15:48 -0400

    Fixed homepage text again

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,10 +4,10 @@ export default function Home() {
 	return (
 		<>
 			<TextBox portraitURL="/assets/keven_portrait.png">
-				hi, i'm keven. i'm a video game music composer and producer, inspired by fusion-jazz.
+				hi, i'm keven. i'm a music composer and producer with a specialty in video game music.
 			</TextBox>
 			<TextBox>
-				you can find various examples of my works, projects that i've worked on, and some info about me on this site.
+				you can find various examples of my works, commission information, and some info about me on this site.
 			</TextBox>
 		</>
 	);
