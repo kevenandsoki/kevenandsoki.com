119033c4cdf45e757b3237d6c94b9f310d18c9f8
kevenandsoki <53108002+kevenandsoki@users.noreply.github.com>
2024-05-03 15:08:01 -0400

    FIXED HOME PAGE AGAIN OH MY GOD HELP ME

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,7 +4,7 @@ export default function Home() {
 	return (
 		<>
 			<TextBox portraitURL="/assets/keven_portrait.png">
-				hi, i'm keven. i'm a NY music composer and producer with a specialty in video game music.
+				hi, i'm keven! i'm a NY music composer with a specialty in VGM, fusion jazz, and EDM.
 			</TextBox>
 			<TextBox>
 				you can find various examples of my works, commission information, and some info about me on this site.
