11b0776c4f615d65353d88af45fbf2fee45790bd
kevenandsoki <53108002+kevenandsoki@users.noreply.github.com>
2024-07-24 01:12:52 -0400

    mail is now ali!

--- components/Footer/index.jsx
+++ components/Footer/index.jsx
@@ -6,7 +6,7 @@ import classNames from 'classnames';
 export default function Footer() {
 	return (
 		<footer className={classNames(styles.footer, fadeInStyles.fadeIn)}>
-			<FooterLink href="mailto:keven@kevenandsoki.com">Email</FooterLink>
+			<FooterLink href="mailto:ali@kevenandsoki.com">Email</FooterLink>
 			<FooterLink href="https://www.youtube.com/@kevenandsoki">YT</FooterLink>
 			<FooterLink href="https://vgen.co/kevenandsoki">VGen</FooterLink>
 			<FooterLink href="https://kevenandsoki.bandcamp.com">Bandcamp</FooterLink>
