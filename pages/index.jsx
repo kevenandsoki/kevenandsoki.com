b88a062dd2b62794a8c40d6935f71967cbdb5b13
kevenandsoki <53108002+kevenandsoki@users.noreply.github.com>
2024-10-14 17:25:23 -0400

    dont call me ali if you dont know me...

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,7 +4,7 @@ export default function Home() {
 	return (
 		<>
 			<TextBox portraitURL="/assets/ali_portrait.png">
-				hi, i'm ali! i'm a NY music composer with a specialty in VGM, fusion jazz, and EDM.
+				hi, i'm alison! i'm a NY music composer with a specialty in VGM, fusion jazz, and EDM.
 			</TextBox>
 			<TextBox>
 				you can find various examples of my works, commission information, and some info about me on this site.
