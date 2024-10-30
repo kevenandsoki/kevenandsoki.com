4aa23687693b0f1a1cd0b419aecd811a00d17781
kevenandsoki <53108002+kevenandsoki@users.noreply.github.com>
2024-10-29 20:58:08 -0400

    footer is cool

--- components/Footer/index.jsx
+++ components/Footer/index.jsx
@@ -1,11 +1,9 @@
 import styles from './styles.module.scss';
-import fadeInStyles from '../../styles/fade_in.module.scss';
 import FooterLink from '../FooterLink';
-import classNames from 'classnames';
 
 export default function Footer() {
 	return (
-		<footer className={classNames(styles.footer, fadeInStyles.fadeIn)}>
+		<footer className={styles.footer}>
 			<FooterLink href="mailto:ali@kevenandsoki.com">Email</FooterLink>
 			<FooterLink href="https://www.youtube.com/@kevenandsoki">YT</FooterLink>
 			<FooterLink href="https://vgen.co/kevenandsoki">VGen</FooterLink>
